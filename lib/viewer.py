92683de739d6875dbae1c22b675725c369a4627f
Tom Fyfe <tomfyfe85@gmail.com>
2023-12-02 15:21:37 +0000

    added viewer class and some notes on methods

---
+++ lib/viewer.py
@@ -0,0 +1,10 @@
+# view is responsible for handling the way output from the 
+# ... other classes is viewed by the user
+
+# add methods which are bespoke for each class?
+# menu 
+# basket.add_to_basket
+# basket.view_basket
+
+# will need to alter existing tests
+class Viewer():
